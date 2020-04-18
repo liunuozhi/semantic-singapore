303c4809a12857284d7cc9f46f16d40f287ef47f
liunuozhi <liunuozhi1995@gmail.com>
2020-04-18 22:10:51 +0800

    nav bar

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # semantic-singapore
 Exploring contextual semantics in Singapore: Using spatial-temporal model for mining POIs and traffic data
+
+## Get started
+
+Install the dependencies...
+
+```bash
+cd semantic-singapore
+npm install
+npm run dev
+```
+
+Navigate to [localhost:5000](http://localhost:5000). You should see your app running. Edit a component file in `src`, save it, and reload the page to see your changes.
+
+By default, the server will only respond to requests from localhost. To allow connections from other computers, edit the `sirv` commands in package.json to include the option `--host 0.0.0.0`.
+
+## Building and running in production mode
+
+To create an optimised version of the app:
+
+```bash
+npm run build
+```
+
+You can run the newly built app with `npm run start`. This uses [sirv](https://github.com/lukeed/sirv), which is included in your package.json's `dependencies` so that the app will work when you deploy to platforms like [Heroku](https://heroku.com).
---
+++ src/App.svelte
@@ -0,0 +1,69 @@
+<script>
+  import Home from "./Home.svelte";
+  import About from "./About.svelte";
+  export let nav = "home";
+
+  let class1, class2;
+
+  $: {
+    if (nav) {
+      if (nav === "home") {
+        class1 = "topnav-active";
+      } else {
+        class1 = "";
+      }
+      if (nav === "about") {
+        class2 = "topnav-active";
+      } else {
+        class2 = "";
+      }
+    }
+  }
+</script>
+
+<main>
+  <ul class="topnav">
+    <a href="/" on:click|preventDefault={() => (nav = 'home')} class={class1}>
+      Home
+    </a>
+    <a href="/" on:click|preventDefault={() => (nav = 'about')} class={class2}>
+      About
+    </a>
+  </ul>
+
+  {#if nav === 'home'}
+    <Home />
+  {:else if nav === 'about'}
+    <About />
+  {/if}
+</main>
+
+<style>
+  .topnav {
+    background-color: #53aeb6;
+    overflow: hidden;
+    margin-bottom: 0;
+    border: 0;
+  }
+
+  /* Style the links inside the navigation bar */
+  .topnav a {
+    float: left;
+    color: #f2f2f2;
+    text-align: center;
+    padding: 14px 16px;
+    text-decoration: none;
+    font-size: 17px;
+  }
+
+  /* Change the color of links on hover */
+  .topnav a:hover {
+    color: rgb(124, 124, 124);
+  }
+
+  /* Add a color to the active/current link */
+  .topnav-active {
+    background-color: #9bd0d5;
+    color: white;
+  }
+</style>
